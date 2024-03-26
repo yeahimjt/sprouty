aeb498f6911492cd1b1cdf82f5106586e5081f74
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-03-25 22:38:12 -0500

    layout complete

---
+++ components/welcome.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Welcome = () => {
+  return (
+    <View style={{ marginTop: 16 }}>
+      <Text style={{ fontSize: 24, color: "#031B0F" }}>Hey, Jonathan 👋</Text>
+      <Text style={{ fontSize: 12, color: "#354F52" }}>
+        Good Morning, Have a Nice Day!
+      </Text>
+    </View>
+  );
+};
+
+export default Welcome;
