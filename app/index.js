738223c9a2ca973fc331e896216167c8b0dc5a10
yeahimjt <JonathanTrevino1327@yahoo.com>
2024-03-29 21:07:05 -0500

    redirecting index to home.js

--- app/index.js
+++ app/index.js
@@ -1,24 +1,5 @@
-import { Text, View, ScrollView } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { Stack, useRouter } from "expo-router";
+import { Redirect } from "expo-router";
 
-const Home = () => {
-  return (
-    <SafeAreaView>
-      <Stack.Screen
-        options={{
-          headerStyle: { backgroundColor: "#FFFFFF" },
-          headerShadowVisible: false,
-          headerShown: false,
-        }}
-      />
-      <ScrollView showsVerticalScrollIndicator={false}>
-        <View>
-          <Text>Hey</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-export default Home;
+export default function Index() {
+  return <Redirect href="/home" />;
+}
